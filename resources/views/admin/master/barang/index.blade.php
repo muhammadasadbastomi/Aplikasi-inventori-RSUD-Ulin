ed450d7b235a2922b819a9131cde9267e89488f4
aryudi33 <33aryudi@gmail.com>
2020-05-31 21:07:34 +0800

    Tambah Stok Barang

--- resources/views/admin/master/barang/index.blade.php
+++ resources/views/admin/master/barang/index.blade.php
@@ -28,7 +28,6 @@
                             <!-- Modal Tambah-->
                             <button class="btn btn-outline-primary" data-toggle="modal" data-target="#modalTambah"><span><i class="feather icon-plus"></i> Tambah
                                     Data</span></button>
-                            @include('admin.master.barang.create')
                             <!-- Modal End -->
                         </div>
                         <div class="table-responsive">
@@ -39,6 +38,7 @@
                                         <th scope="col" class="text-center">Nama Barang</th>
                                         <th scope="col" class="text-center">Merk</th>
                                         <th scope="col" class="text-center">Satuan</th>
+                                        <th scope="col" class="text-center">Stok Barang</th>
                                         <th scope="col" class="text-center">Aksi</th>
                                     </tr>
                                 </thead>
@@ -49,11 +49,12 @@
                                         <td scope="col" class="text-center">{{ $b->nama_barang }}</td>
                                         <td scope="col" class="text-center">{{ $b->merk->nama_merk }}</td>
                                         <td scope="col" class="text-center">{{ $b->satuan->nama_satuan }}</td>
+                                        <td scope="col" class="text-center">{{ $b->stok }}</td>
                                         <td scope="col" class="text-center">
-                                            <a class="btn btn-sm btn-info text-white" data-id="{{$b->id}}" data-nama_barang="{{$b->nama_barang}}" data-merk_id="{{$b->merk->id}}" data-satuan_id="{{$b->satuan->id}}" data-toggle="modal" data-target="#editModal">
+                                            <a class="btn btn-sm btn-info text-white" data-id="{{$b->id}}" data-nama_barang="{{$b->nama_barang}}" data-merk_id="{{$b->merk->id}}" data-satuan_id="{{$b->satuan->id}}" data-stok="{{$b->stok}}" data-toggle="modal" data-target="#editModal">
                                                 <i class="fa fa-pencil color-muted m-r-5"></i>
                                             </a>
-                                            <a href="#" data-toggle="tooltip" data-placement="top" title="" data-original-title="Close"><i class="fa fa-close color-danger"></i></a>
+                                            <a class="btn btn-sm btn-danger text-white" href="#" data-toggle="tooltip" data-placement="top"><i class="fa fa-close color-danger"></i></a>
                                         </td>
                                     </tr>
                                     @endforeach
@@ -68,55 +69,8 @@
     <!-- #/ container -->
     </div>
 
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="edit-modal-label" aria-hidden="true">
-        <div class="modal-dialog modal-xl" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="edit-modal-label" style="padding-left: 10px;">Edit Barang</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form method="post">
-                        {{ method_field('put') }}
-                        @csrf
-                        <div class=" modal-body">
-                            <input type="hidden" name="id" id="id">
-                            <div class="form-group">
-                                <label class="col-form-label" for="nama_barang">Nama Barang</label>
-                                <input type="text" class="form-control @error ('nama_barang') is-invalid @enderror" placeholder="Masukkan Nama Barang" name="nama_barang" value="{{old('nama_barang')}}" id="nama_barang" autofocus>
-                                @error('nama_barang')<div class="invalid-feedback"> {{$message}} </div>@enderror
-                            </div>
-
-                            <div class="form-group">
-                                <label for="merk_id">Merk</label>
-                                <select class="custom-select" name="merk_id" id="merk_id">
-                                    @foreach($merk as $d)
-                                    <option value="{{$d->id}}">{{$d->nama_merk}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="satuan_id">Satuan</label>
-                                <select class="custom-select" name="satuan_id" id="satuan_id">
-                                    @foreach($satuan as $d)
-                                    <option value="{{$d->id}}">{{$d->nama_satuan}}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                        </div>
-                        <div class="edit modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="edit btn btn-primary">Ubah</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('admin.master.barang.create')
+    @include('admin.master.barang.edit')
     @endsection
 
     @section('script')
@@ -131,12 +85,14 @@
             var nama_barang = button.data('nama_barang')
             var merk_id = button.data('merk_id')
             var satuan_id = button.data('satuan_id')
+            var stok = button.data('stok')
             var modal = $(this)
 
             modal.find('.modal-body #id').val(id)
             modal.find('.modal-body #nama_barang').val(nama_barang);
             modal.find('.modal-body #merk_id').val(merk_id);
             modal.find('.modal-body #satuan_id').val(satuan_id);
+            modal.find('.modal-body #stok').val(stok);
         })
     </script>
 

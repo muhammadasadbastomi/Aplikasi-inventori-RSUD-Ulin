7a2c0a7e3bf4726715fdaa25fa66b0f0539a7472
aryudi33 <33aryudi@gmail.com>
2020-05-31 18:14:17 +0800

    Renew Sidebar n Navbar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,57 +1,26 @@
 <div class="nk-sidebar">
     <div class="nk-nav-scroll">
         <ul class="metismenu" id="menu">
-            <li class="nav-label">Dashboard</li>
             <li>
                 <a href="{{route('dashboard')}}">
-                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
-                </a>
-            </li>
-            <li class="nav-label">Master Data</li>
-            <li>
-                <a href="{{route('barangIndex')}}">
-                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Data Barang</span>
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text"> Dashboard </span>
                 </a>
             </li>
             <li>
-                <a href="{{route('merkIndex')}}">
-                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Data Merk</span>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-notebook menu-icon"></i><span class="nav-text"> Master Data </span>
                 </a>
-            </li>
-            <li>
-                <a href="{{route('unitIndex')}}">
-                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Data Unit</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{route('satuanIndex')}}">
-                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Data Satuan</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{route('supplierIndex')}}">
-                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Data Supplier</span>
-                </a>
+                <ul aria-expanded="false">
+                    <li><a href="{{route('barangIndex')}}" aria-expanded="false">Data Barang</a></li>
+                    <li><a href="{{route('merkIndex')}}" aria-expanded="false">Data Merk</a></li>
+                    <li><a href="{{route('unitIndex')}}" aria-expanded="false">Data Unit</a></li>
+                    <li><a href="{{route('satuanIndex')}}" aria-expanded="false">Data Satuan</a></li>
+                    <li><a href="{{route('supplierIndex')}}" aria-expanded="false">Data Suplier</a></li>
+                </ul>
             </li>
             <li>
                 <a href="#">
-                    <i class="icon-screen-tablet menu-icon"></i><span class="nav-text">Data</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="icon-graph menu-icon"></i> <span class="nav-text">Data</span>
-                </a>
-            </li>
-            <li class="nav-label">Setting</li>
-            <li>
-                <a href="#">
-                    <i class="icon-grid menu-icon"></i><span class="nav-text">Admin</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="icon-badge menu-icon"></i><span class="nav-text">Widget</span>
+                    <i class="icon-user menu-icon"></i><span class="nav-text"> Admin </span>
                 </a>
             </li>
         </ul>
